6789e049f4d685456110ebac155aaa0996a422e9
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2022-02-09 19:04:36 +0200

    add functionality to the AboutMeActivity.kt

--- app/src/main/java/com/example/twoactivity/AboutMeActivity.kt
+++ app/src/main/java/com/example/twoactivity/AboutMeActivity.kt
@@ -1,11 +1,29 @@
 package com.example.twoactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.StringRes
+import com.example.twoactivity.databinding.ActivityAboutMeBinding
 
 class AboutMeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAboutMeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_me)
+        binding = ActivityAboutMeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.returnBtn.setOnClickListener {
+            toast(R.string.toast_message)
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun toast(mess: Int) {
+        val toast = Toast.makeText(this, mess, Toast.LENGTH_SHORT).show()
     }
 }
