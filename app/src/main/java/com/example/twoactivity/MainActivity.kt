6cacd7b661712332f91deed9cf349521e541be4c
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2022-02-11 13:42:58 +0200

    create functionality in MainActivity.kt to receive data from AboutMeActivity

--- app/src/main/java/com/example/twoactivity/MainActivity.kt
+++ app/src/main/java/com/example/twoactivity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.twoactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import com.example.twoactivity.databinding.ActivityMainBinding
@@ -17,11 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.firstActivityBtn.setOnClickListener {
-            openActivityAboutMe(it)
+            openActivityAboutMe()
         }
     }
 
-    private fun openActivityAboutMe(view: View) {
+    private fun openActivityAboutMe() {
         // get user input data
         val userName = binding.userName.text.toString()
         val userCity = binding.userCity.text.toString()
@@ -32,13 +33,48 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(EXTRA_NAME, userName)
         intent.putExtra(EXTRA_City, userCity)
         // start our activity with intent with data, which we pass into intent
-        startActivity(intent)
+//        startActivity(intent)
 
+        Log.i("TAG", "Call intent from MainActivity")
+        startActivityForResult(intent, POSITIVE_REQUEST_CODE)
         overridePendingTransition(R.anim.slide_right, R.anim.slide_out_right)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == POSITIVE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                val resultAboutMeName = data?.getStringExtra("resultName")
+                val resultAboutMeCity = data?.getStringExtra("resultCity")
+
+                Log.i("TAG", "$resultAboutMeName, $resultAboutMeCity")
+                if (resultAboutMeName.equals("") && resultAboutMeCity.equals("")){
+                    binding.activityOne.text = resources.getString(R.string.hello_i_m_activity_one)
+                }
+                else if (resultAboutMeName.equals("") || resultAboutMeCity.equals("")){
+                    when {
+                        resultAboutMeName.equals("") ->
+                            binding.activityOne.text = resources
+                                .getString(R.string.resultAboutMeNameCity,
+                                    "Nobody", resultAboutMeCity)
+                        resultAboutMeCity.equals("") ->
+                            binding.activityOne.text = resources
+                                .getString(R.string.resultAboutMeNameCity,
+                                     resultAboutMeName, "NewerLand")
+                    }
+                } else {
+                    binding.activityOne.text = resources
+                        .getString(R.string.resultFromAboutMe, resultAboutMeName, resultAboutMeCity)
+                }
+            }
+        }
     }
 
     companion object {
         const val EXTRA_NAME = "Name"
         const val EXTRA_City = "City"
+
+        const val POSITIVE_REQUEST_CODE = 1
     }
 }
